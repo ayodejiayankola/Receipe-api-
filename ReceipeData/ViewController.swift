02deb033329792fc2a2eeb15aae9bd43c9a11649
ayodejiayankola <ayodejiayankola@gmail.com>
2020-08-01 15:10:02 +0100

    Initial Commit

---
+++ ReceipeData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReceipeData
+//
+//  Created by Ayodeji Ayankola on 8/1/20.
+//  Copyright © 2020 konga.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
